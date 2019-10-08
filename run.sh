15909bf222b3bc75f820845ee44cad76e98cf829
mackay2k4 <sheppard2k@gmail.com>
2019-10-08 13:10:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-  /usr/local/bin/r53dyndns.py -v -R $ROUTE53_DOMAIN_A_RECORDS -ip6R $ROUTE53_DOMAIN_AAAA_RECORDS -T $ROUTE53_RECORD_TTL
+  /usr/local/bin/r53dyndns.py -v -R $ROUTE53_DOMAIN_A_RECORDS -T $ROUTE53_RECORD_TTL
   echo "Sleeping for $ROUTE53_UPDATE_FREQUENCY seconds..."
   sleep $ROUTE53_UPDATE_FREQUENCY
 done
