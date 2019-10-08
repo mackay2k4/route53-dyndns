faba7c2eae385e70d8dc128e013781a2dc67000b
mackay2k4 <sheppard2k@gmail.com>
2019-10-08 13:08:17 +0200

    Update r53dyndns.py

--- r53dyndns.py
+++ r53dyndns.py
@@ -13,7 +13,6 @@ import sys
 
 parser = OptionParser()
 parser.add_option('-R', '--record', type='string', dest='records_to_update', help='The A record(s) to update (comma-separated).')
-parser.add_option('-ip6R', '--record', type='string', dest='ip6records_to_update', help='The A record(s) to update (comma-separated).')
 parser.add_option('-v', '--verbose', dest='verbose', default=False, help='Enable Verbose Output.', action='store_true')
 parser.add_option('-T', '--ttl', type='int', dest='ttl', default=300, help='Set record TTL in seconds (default 300).')
 (options, args) = parser.parse_args()
@@ -38,14 +37,13 @@ for rdata in resolver.query('myip.opendns.com', 'AAAAA'):
     logging.info('Current IP address: %s', current_ipv6)
     
 records_to_update = options.records_to_update.split(',')
-ip6records_to_update = options.ip6records_to_update.split(',')
 zone_to_update = '.'.join(records_to_update[0].split('.')[-2:])
 
 try:
     socket.inet_aton(current_ip)
     conn = boto.route53.connect_to_region(os.getenv('AWS_CONNECTION_REGION', 'eu-west-1'))
     zone = conn.get_zone(zone_to_update)
-    for ip6record_to_update in ip6records_to_update:
+    for ip6record_to_update in records_to_update:
         ip6record_updated = False
         for ip6record in zone.get_records():
             # substitute '*' with '\052' due to boto weirdness
